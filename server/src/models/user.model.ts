a033cb8f9b369a2c4bf3694985ff0af277978c4d
benshabbat <benshabbat27@gmail.com>
2025-05-04 11:45:43 +0300

    Refactor user model to use consistent import statements and ensure password is required

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,19 +1,19 @@
-import mongoose, { Schema } from 'mongoose';
-import { IUser } from '../interfaces/user.interface';
+import { Schema, model } from "mongoose";
+import { IUser } from "../interfaces/user.interface";
 
 const UserSchema: Schema = new Schema(
-    {
-        firstName: { type: String, required: true },
-        lastName: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
-        phone: { type: String, required: true },
-        password: { type: String, required: false },
-        role: { type: String, required: true, enum: ['admin', 'user'] },
-    },
-    {
-        timestamps: true,
-    }
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true, enum: ["admin", "user"] },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const UserModel = mongoose.model<IUser>('User', UserSchema);
+const UserModel = model<IUser>("User", UserSchema);
 export default UserModel;
